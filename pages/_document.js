1dffb7a5404b51ee81fd296cf56f6a9fd75268a2
DriptaSenapati <driptasenapati97@gmail.com>
2022-07-07 12:55:45 +0530

    Code Change

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
+
   render() {
+    const isProd = process.env.NODE_ENV === 'production';
+
     return (
       <Html>
         <Head>
-          <link rel="manifest" href="/manifest.json"/>
-          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <link rel="manifest" href={isProd ? `${process.env.NEXTJS_PUBLIC_FAVICON}/manifest.json` : '/manifest.json'}/>
+          <link rel="apple-touch-icon" href={isProd ? `${process.env.NEXTJS_PUBLIC_FAVICON}/icon.png` : '/icon.png'}></link>
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
