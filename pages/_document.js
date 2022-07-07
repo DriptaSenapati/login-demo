fc89a57bf9712b243581c542ff65733e1d556cca
DriptaSenapati <driptasenapati97@gmail.com>
2022-07-07 12:46:35 +0530

    Code Change

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-var repo_name = "/logindemo"
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel="manifest" href={repo_name+"/manifest.json"} />
-          <link rel="apple-touch-icon" href={repo_name+"/icon.png"}></link>
+          <link rel="manifest" href="/manifest.json"/>
+          <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
