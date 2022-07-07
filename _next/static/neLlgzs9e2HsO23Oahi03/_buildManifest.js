8fd95fde12ad6c3360694905ddff4d55453edb2c
DriptaSenapati <63300981+DriptaSenapati@users.noreply.github.com>
2022-07-07 13:26:42 +0000

    Deploying to gh-pages from @ DriptaSenapati/login-demo@dfe7e208810ada4a461bdc6f6f426bf14a6ce56a 🚀

---
+++ _next/static/neLlgzs9e2HsO23Oahi03/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/95ed801f174227fe.css","static/chunks/pages/index-a2e70403af02a1c2.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
