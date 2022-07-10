363a4bd0b2247ea7e0fe4a989087deb1d0611e91
DriptaSenapati <driptasenapati97@gmail.com>
2022-07-10 21:50:41 +0530

    Auth Change

--- pages/products.js
+++ pages/products.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 
 const products = () => {
   return (
     <div>
-      <Navbar/>
       products
     </div>
   )
--- pages/services.js
+++ pages/services.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 
 const services = () => {
   return (
     <div>
-      <Navbar/>
       services
     </div>
   )
